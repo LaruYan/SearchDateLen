df5f49144bdf959a84afcfdd4d2facd5eb75b33c
MSM8994 <41772872+SD810@users.noreply.github.com>
2019-01-21 11:44:22 +0900

    Utils: create getClassedTag, check DOM obj

--- utils.js
+++ utils.js
@@ -26,3 +26,34 @@ function unescapeHtml(safe) {
         .replace(/&quot;/g, '"')
         .replace(/&#039;/g, "'");
 }
+
+// div/span 태그에 클래스를 정해둔 DOM 객체를 반환
+// inner를 내부에 넣음
+function getClassedTag(tagType, className, inner){
+    var tag = document.createElement(tagType);
+    tag.setAttribute('class', className);
+    if(isNode(inner) || isElement(inner)){
+        tag.appendChild(inner);
+    }else{
+        tag.innerHTML = inner;
+    }
+    return tag;
+}
+
+
+
+//https://stackoverflow.com/a/384380
+// DOM Node는 true
+function isNode(o){
+    return (
+        typeof Node === "object" ? o instanceof Node : 
+        o && typeof o === "object" && typeof o.nodeType === "number" && typeof o.nodeName==="string"
+    );
+}  
+// DOM Element는 true    
+function isElement(o){
+    return (
+        typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+        o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
+    );
+}
