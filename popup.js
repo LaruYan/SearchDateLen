2eaa473e4b9bd4906918ced1938ab974682f85cc
MSM8994 <41772872+SD810@users.noreply.github.com>
2019-02-17 17:41:50 +0900

    add missing version number to popup

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 // 확장기능 내에서 inline javascript가 동작하지 않으므로 직접 이벤트 리스너로 추가해야한다
 document.addEventListener('DOMContentLoaded', function () {
 
+    document.getElementById('versionNo').innerHTML = chrome.runtime.getManifest().version;
+
     document.getElementById('openOptionsPage').addEventListener('click', function () {
         chrome.runtime.openOptionsPage();
     });
