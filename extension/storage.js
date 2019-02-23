454e3c38838315c63a3d523652c5754cdf2f38c0
LaruYan KaPothoa <laruyan@users.noreply.github.com>
2019-02-23 18:07:27 +0900

    add reset button

--- extension/storage.js
+++ extension/storage.js
@@ -11,16 +11,24 @@ var datesJsonStr = null;
 // 저장된 데이터 (obj)
 var datesData = null;
 
+
+/**
+ * 초기 값을 설정합니다.
+ * @param {function} callback
+ */
+function initializeSearchDateLen(callback = null){
+    datesJsonStr = '[{"name":"3M","type":"rel_from","from_year":"0","from_month":"3","from_date":"0","to_year":"0","to_month":"0","to_date":"0"},{"name":"6M","type":"rel_from","from_year":"0","from_month":"6","from_date":"0","to_year":"0","to_month":"0","to_date":"0"},{"name":"H1","type":"abs_range","from_year":"0","from_month":"1","from_date":"1","to_year":"0","to_month":"6","to_date":"30"},{"name":"H2","type":"abs_range","from_year":"0","from_month":"7","from_date":"1","to_year":"0","to_month":"12","to_date":"31"},{"name":"1Q","type":"abs_range","from_year":"0","from_month":"1","from_date":"1","to_year":"0","to_month":"3","to_date":"31"},{"name":"2Q","type":"abs_range","from_year":"0","from_month":"4","from_date":"1","to_year":"0","to_month":"6","to_date":"30"},{"name":"3Q","type":"abs_range","from_year":"0","from_month":"7","from_date":"1","to_year":"0","to_month":"9","to_date":"30"},{"name":"4Q","type":"abs_range","from_year":"0","from_month":"10","from_date":"1","to_year":"0","to_month":"12","to_date":"31"}]';
+    saveStorage(callback);
+}
+
 /**
  * 스토리지로부터 불러온 값을 확인하고 없으면 기본 값으로 초기화
  */
 function checkDataAndForceDefault(){
     if(!datesJsonStr || datesJsonStr == undefined || datesJsonStr == null)
     {
-        
         alertAndLog('searchDateLen: [storage] Json not found. force default');
-        datesJsonStr = '[{"name":"3M","type":"rel_from","from_year":"0","from_month":"3","from_date":"0","to_year":"0","to_month":"0","to_date":"0"},{"name":"6M","type":"rel_from","from_year":"0","from_month":"6","from_date":"0","to_year":"0","to_month":"0","to_date":"0"},{"name":"H1","type":"abs_range","from_year":"0","from_month":"1","from_date":"1","to_year":"0","to_month":"6","to_date":"30"},{"name":"H2","type":"abs_range","from_year":"0","from_month":"7","from_date":"1","to_year":"0","to_month":"12","to_date":"31"},{"name":"1Q","type":"abs_range","from_year":"0","from_month":"1","from_date":"1","to_year":"0","to_month":"3","to_date":"31"},{"name":"2Q","type":"abs_range","from_year":"0","from_month":"4","from_date":"1","to_year":"0","to_month":"6","to_date":"30"},{"name":"3Q","type":"abs_range","from_year":"0","from_month":"7","from_date":"1","to_year":"0","to_month":"9","to_date":"30"},{"name":"4Q","type":"abs_range","from_year":"0","from_month":"10","from_date":"1","to_year":"0","to_month":"12","to_date":"31"}]';
-        saveStorage();
+        initializeSearchDateLen();
     }
 }
 
@@ -42,8 +50,10 @@ function saveStorage(callback){
     chrome.storage.sync.set({
         dates: datesJsonStr
     }, function() {
-        alertAndLog('searchDateLen: [storage] calling callback after saveStorage()');
-        callback.call();
+        if(callback){
+            alertAndLog('searchDateLen: [storage] calling callback after saveStorage()');
+            callback.call();
+        }
     });
 }
 
@@ -56,9 +66,10 @@ function loadStorage(callback){
     //크롬 스토리지에 저장된 JSON값을 가져온다. 
     chrome.storage.sync.get(function (data) {
         datesJsonStr = data.dates;
-
-        alertAndLog('searchDateLen: [storage] calling callback after loadStorage()');
-        callback.call();
+        if(callback){
+            alertAndLog('searchDateLen: [storage] calling callback after loadStorage()');
+            callback.call();
+        }
     });
 }
 
